544a2c02cd969d5fcc7b1593be09a2adf1b92988
Moon Jun Sung <leo8689123@naver.com>
2026-06-02 10:18:08 +0900

    Update validation script style

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -1,58 +1,52 @@
-function menu2Check() {
+function insertCheck(){
 	var frm = document.frm;
-
-	if (frm.sid.value.trim() == "") {
+	
+	if(frm.sid.value === ""){
 		alert("학번이 입력되지 않았습니다!");
 		frm.sid.focus();
 		return false;
 	}
-
-	if (frm.midscore.value.trim() == "" || isNaN(frm.midscore.value)) {
+	if(frm.midscore.value === "" || isNaN(frm.midscore.value)){
 		alert("중간고사는 숫자가 아니거나 입력하지 않았습니다!");
 		frm.midscore.focus();
 		return false;
 	}
-
-	if (frm.finalscore.value.trim() == "" || isNaN(frm.finalscore.value)) {
+	if(frm.finalscore.value === "" || isNaN(frm.finalscore.value)){
 		alert("기말고사는 숫자가 아니거나 입력하지 않았습니다!");
 		frm.finalscore.focus();
 		return false;
 	}
-
-	if (frm.attend.value.trim() == "" || isNaN(frm.attend.value)) {
+	if(frm.attend.value === "" || isNaN(frm.attend.value)){
 		alert("출석은 숫자가 아니거나 입력하지 않았습니다!");
 		frm.attend.focus();
 		return false;
 	}
-
-	if (frm.report.value.trim() == "" || isNaN(frm.report.value)) {
+	if(frm.report.value === "" || isNaN(frm.report.value)){
 		alert("레포트는 숫자가 아니거나 입력하지 않았습니다!");
 		frm.report.focus();
 		return false;
 	}
-
-	if (frm.etc.value.trim() == "" || isNaN(frm.etc.value)) {
+	if(frm.etc.value === "" || isNaN(frm.etc.value)){
 		alert("기타는 숫자가 아니거나 입력하지 않았습니다.");
 		frm.etc.focus();
 		return false;
 	}
-
-	return true;
+	alert("성적정보가 정상적으로 등록되었습니다!");
+	frm.submit();
 }
 
-function menu2Reset() {
+function reset(){
 	alert("정보를 지우고 처음부터 다시 입력합니다!");
-	document.frm.reset();
+	location.href="menu2.jsp";
 }
 
-function menu4Check() {
+function menu4Check(){
 	var frm = document.frm;
-
-	if (frm.subcode.value.trim() == "") {
+	
+	if(frm.subcode.value === ""){
 		alert("과목코드가 입력되지 않았습니다!");
 		frm.subcode.focus();
 		return false;
 	}
-
 	return true;
 }
